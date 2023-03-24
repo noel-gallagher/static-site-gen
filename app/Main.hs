e3c751f3b67a57409525134f1c0ea85cf08f6d97
noel-gallagher <noel.gallagher@ppb.com>
2023-03-24 18:23:56 +0000

    refactor main

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,16 @@
 import Html
-
+main :: IO ()
 main = putStrLn (render myPage)
 
 myPage :: Html
-myPage = html_ "my page"
-    (append_
-        (h1_ "Hello")
-        (append_
-            (p_ "Welcome to")
-            (p_ "my page")
-        )
-    )
-
+myPage =
+    let
+        title = "my page"
+        content = append_
+            (h1_ "Hello")
+            (append_
+                (p_ "Welcome to")
+                (p_ "my page")
+            )
+    in html_ title content
 
