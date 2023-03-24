5e240beb7a93b181b7b50b95c64e60c1132bdc76
noel-gallagher <noel.gallagher@ppb.com>
2023-03-24 20:13:55 +0000

    add test for header

--- test/Tests.hs
+++ test/Tests.hs
@@ -5,9 +5,18 @@ prop_p :: String -> Bool
 prop_p content =
   let
     paragraph = p_ content
-    expected = "<p>" <> content ++ "</p>"
+    expected = "<p>" <> content <> "</p>"
   in
      show paragraph == expected
+
+prop_h1 :: Title -> Bool
+prop_h1 title = 
+  let
+    header = h1_ title 
+    expected = "<h1>" <> title <> "</h1>" 
+  in
+    show header == expected
+
 
 main :: IO ()
 main = quickCheck prop_p
