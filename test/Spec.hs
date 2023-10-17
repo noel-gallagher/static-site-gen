b9d2673b4e178d8ecd662fae6db32b9820d0a393
noel-gallagher <noel.gallagher164@gmail.com>
2023-10-17 07:10:02 +0100

    refactor test

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,44 +5,36 @@ import Html
 specialCharsToExclude :: [Char]
 specialCharsToExclude = "<>&\"\'"
 
-stringWithoutConverted :: Gen String
-stringWithoutConverted = listOf $ elements $ filter (`notElem` specialCharsToExclude) [' ' .. '-']
+genStringWithoutConverted :: Gen String
+genStringWithoutConverted = listOf $ elements $ filter (`notElem` specialCharsToExclude) [' ' .. '-']
 
-prop_p :: String -> Bool
-prop_p content =
-  let
-    paragraph = p_ content
-    expected = "<p>" <> content <> "</p>"
-  in
-     getStructureString paragraph == expected
+prop_wrapInParagraph :: String -> Bool
+prop_wrapInParagraph content =
+  getStructureString (p_ content) == "<p>" <> content <> "</p>"
 
-prop_h1 :: Title -> Bool
-prop_h1 title = 
-  let
-    header = h_ 1 title 
-    expected = "<h1>" <> title <> "</h1>" 
-  in
-    getStructureString header == expected 
+prop_wrapInHeader :: String -> Bool
+prop_wrapInHeader title =
+  getStructureString (h_ 1 title) == "<h1>" <> title <> "</h1>"
 
-testEscapeChar :: (String, String) -> SpecWith ()
-testEscapeChar (input, expected) =
-  it ("correctly escapes the character " ++ input) $ 
-   escape input `shouldBe` expected 
+specEscapeChar :: (String, String) -> SpecWith ()
+specEscapeChar (input, expected) = 
+  it ("correctly escapes the character " ++ input) $
+    escape input `shouldBe` expected 
 
 main :: IO ()
 main = hspec $ do 
   describe "html gen" $ do
-    it "should wrap paragraphs" $ do
-      property $ forAll stringWithoutConverted $ \str -> prop_p str `shouldBe` True
+    it "should wrap paragraphs" $
+      property $ forAll genStringWithoutConverted prop_wrapInParagraph
 
-    it "should wrap h1" $ do
-      property $ forAll stringWithoutConverted $ \str -> prop_h1 str `shouldBe` True
+    it "should wrap h1" $
+      property $ forAll genStringWithoutConverted prop_wrapInHeader
     
     let escapeChars = [("<", "&lt"),
-                        (">", "&gt"),
-                        ("\"", "&quot"),
-                        ("\'", "&#39"),
-                        ("&", "&amp")]
-    mapM_ testEscapeChar escapeChars 
+                       (">", "&gt"),
+                       ("\"", "&quot"),
+                       ("\'", "&#39"),
+                       ("&", "&amp")]
 
+    mapM_ specEscapeChar escapeChars
 
